d37a595061a7e5cd68aa0b42e571269440a37ea5
2199robolions <robolionsgit@gmail.com>
2024-01-15 16:56:31 -0500

    Start implementing auto functionality

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public class Auto {
     // State tracking variables - each variable can only be used in one function at any time
     // All top level routines use firstTime and step, all helper routines have their own variables
@@ -23,6 +26,50 @@ public class Auto {
         this.drive = drive;
         this.position = position;
         this.nTables  = CustomTables.getInstance();
+    }
+
+    public int testAuto(long delaySeconds) {
+        int    status   = Robot.CONT;
+        Pose2d currPose = position.getOdometryPose();
+
+        if (firstTime == true) {
+			firstTime = false;
+			step = 1;
+            System.out.println("Starting test auto");
+		}
+
+        switch(step) {
+            case 1:
+                // Delay
+                status = autoDelay(delaySeconds);
+                drive.rotateWheels(1, 0, 0);  
+                break;
+            
+            case 2:
+                status = drive.rotateWheels(1, 0, 0);
+                break;
+
+            case 3:
+                Pose2d points = new Pose2d(1, 0, new Rotation2d(0));
+                status = drive.autoDriveToPoints(new Pose2d[]{points}, currPose);
+                break;
+
+            default:
+                // Finished routine
+                step = 1;
+                firstTime = true;
+
+                // Stops applicable motors
+                drive.stopWheels();
+                return Robot.DONE;
+        }
+
+        // If we are done with a step, we go on to the next one and continue the routine
+        if (status == Robot.DONE) {
+            step++;
+        }
+
+        return Robot.CONT;
     }
 
 
