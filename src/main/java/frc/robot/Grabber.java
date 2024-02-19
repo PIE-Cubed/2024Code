90699bbe450754e9952243a143fed1c95314e817
Ares <robolionsgit@gmail.com>
2024-02-19 12:40:39 -0500

    Added color sensor and another motor(untested)

--- src/main/java/frc/robot/Grabber.java
+++ src/main/java/frc/robot/Grabber.java
@@ -5,18 +5,36 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.MathUtil;
 
-public class Grabber {
-    private final int GRABBER_MOTOR_CAN = 0;
-    private final boolean GRABBER_MOTOR_IS_INVERTED = false;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.util.Color;
+import com.revrobotics.ColorSensorV3;
 
+public class Grabber {
+    private final int GRABBER_MOTOR1_CAN = 2;
+    private final int GRABBER_MOTOR2_CAN = 5;
+
+    private final boolean GRABBER_MOTOR1_IS_INVERTED = false;
+    private final boolean GRABBER_MOTOR2_IS_INVERTED = true;
+
+    // TODO tune these power values
     public final double INTAKE_POWER = 0.75;
     public final double OUTTAKE_POWER = 0.75;
-
-    private CANSparkMax grabberMotor;
+    public final double FEED_POWER = 0.75;
+    
+    public final double IR_THRESHOLD = 200;
+    
+    private CANSparkMax grabberMotor1;
+    private CANSparkMax grabberMotor2;
+    private ColorSensorV3 colorSensor;
     
     public Grabber() {
-        grabberMotor = new CANSparkMax(GRABBER_MOTOR_CAN, MotorType.kBrushed);
-        grabberMotor.setInverted(GRABBER_MOTOR_IS_INVERTED);
+        grabberMotor1 = new CANSparkMax(GRABBER_MOTOR1_CAN, MotorType.kBrushed);
+        grabberMotor1.setInverted(GRABBER_MOTOR1_IS_INVERTED);
+
+        grabberMotor2 = new CANSparkMax(GRABBER_MOTOR2_CAN, MotorType.kBrushed);
+        grabberMotor2.setInverted(GRABBER_MOTOR2_IS_INVERTED);
+
+        colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
     } 
 
     /**
@@ -25,17 +43,69 @@ public class Grabber {
      * @param power
      */
     public void setPower(double power) {
-        grabberMotor.set(MathUtil.clamp(power, -1, 1));
+        grabberMotor1.set(MathUtil.clamp(power, -1, 1));
+        grabberMotor2.set(MathUtil.clamp(power, -1, 1));
     }
 
-    public void intakeOutake(boolean intake, boolean outtake) {
+    /**
+     * <p> Sets the motor power to feed power
+     * <p> Cuts power when a note is detected
+     * 
+     * @return Robot status, CONT if there is no note, DONE if there is
+     * 
+     */
+    // TODO test color sensor's ability to stop intake correctly
+    public int intakeOutake(boolean intake, boolean outtake) {
         if(intake) {
-            setPower(INTAKE_POWER);
+            if(noteDetected()) {
+                return Robot.DONE;
+            } else {
+                setPower(INTAKE_POWER);
+                return Robot.CONT;
+            }
         } else if(outtake) {
             setPower(OUTTAKE_POWER);
+            return Robot.CONT;
         }
         else {
             setPower(0.0);
+            return Robot.DONE;
         }
     }
+
+    /**
+     * <p> Checks if the the color sensor senses a note
+     * <p> Uses a threshold to determine when to cut power
+     * <p> Initial values are ~110, and ~250 with a note
+     * 
+     * @return whether a note is found
+     */
+    public boolean noteDetected() {
+        return colorSensor.getIR() > IR_THRESHOLD;
+    }
+
+    /**
+     * Prints color(r,g,b), ir, and proximity readings from the sensor
+     * IR has a range, from 110 with no note, to 250 with note
+     * Red goes from 0.28 with no note to 0.38 with note
+     * IR works best due to its high range/delta
+     */
+    public void testColorSensor() {
+        Color detectedColor = colorSensor.getColor();
+        
+        double r = detectedColor.red;
+        double g = detectedColor.green;
+        double b = detectedColor.blue;
+        double ir = colorSensor.getIR();
+        double proximity = colorSensor.getProximity();
+        
+        System.out.println( "COLOR SENSOR" +
+            " | R: "  + r  +
+            " | G: "  + g  +
+            " | B: "  + b  +
+            " | IR: " + ir +
+            " | Proximity: " + proximity
+        );
+    }
+
 }
