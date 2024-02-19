e10807a62f6364229baad38fa035a180d2a453de
Ares <robolionsgit@gmail.com>
2024-02-19 12:41:44 -0500

    Added arm autos, only templates for amp and climb autos

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -16,21 +16,183 @@ public class Auto {
     // Object Creation
     private Drive          drive;
     private PoseEstimation position;
+    private Arm            arm;
     private CustomTables   nTables;
 
     // Constructor
-    public Auto(Drive drive, PoseEstimation position) {
-        this.drive = drive;
+    public Auto(Drive drive, PoseEstimation position, Arm arm) {
+        this.drive    = drive;
         this.position = position;
+        this.arm      = arm;
         this.nTables  = CustomTables.getInstance();
+    }
+
+    /****************************************************************************************** 
+     * 
+     *    DRIVE FUNCTIONS
+     * 
+     ******************************************************************************************/    
+
+    /**
+     * Shoot the trap? need to confirm
+     * 
+     * @return Robot status, CONT or DONE
+     */
+    public int trapSequence() { return Robot.CONT; }
+
+    /****************************************************************************************** 
+     * 
+     *    MANIPULATOR FUNCTIONS
+     * 
+     ******************************************************************************************/
+    
+    /**
+     * <p>Moves the arm back into position to pick up a note from the ground
+     * <p> -33.5 degrees from horizontal
+     * <p> 5inch extension of the arm
+     * <p> First rotates, then extends the arm
+     * @return Robot status, CONT or DONE
+     */
+    public int groundPickupPosition() {
+        int status = Robot.CONT;
+
+        if(firstTime == true) {
+            firstTime = false;
+            step = 1;
+        }
+
+        switch(step) {
+            case 1:     // Rotate arm to -33.5 degrees from horizontal
+                status = arm.rotateArm(Math.toDegrees(-33.5));
+                break;
+            case 2:     // Extend arm to 5in(current function takes encoder rotations right now)
+                status = arm.extendArm(1);  // TODO get actual distance
+                break;
+            default:    // Finished routine, reset variables and return done
+                step = 1;
+                firstTime = true;
+                return Robot.DONE;
+        }
+
+        // Done current step, goto next one
+        if(status == Robot.DONE) {
+            step++;
+        }
+
+        return Robot.CONT;
+    }
+
+    /**
+     * <p> Moves the arm back into resting position
+     * <p> -27 degrees from horizontal
+     * <p> Fully retracts arm
+     * <p> First rotates, then retracts the arm
+     * @return Robot status, CONT or DONE
+     */
+    public int restingPosition() {
+        int status = Robot.CONT;
+
+        if(firstTime == true) {
+            firstTime = false;
+            step = 1;
+        }
+
+        switch(step) {
+            case 1:     // Rotate arm to -27 degrees from horizontal
+                status = arm.rotateArm(Math.toDegrees(-27));
+                break;
+            case 2:     // Fully retract arm
+                status = arm.extendArm(0);
+                break;
+            default:    // Finished routine, reset variables and return done
+                step = 1;
+                firstTime = true;
+                return Robot.DONE;
+        }
+
+        // Done current step, goto next one
+        if(status == Robot.DONE) {
+            step++;
+        }
+
+        return Robot.CONT;
+    }
+
+    /**
+     * Moves the arm into the position to shoot into the amp
+     * TODO get arm angle and extension measurments from austin
+     */
+    public int ampPosition() {
+        System.err.println("Not implemented yet!");
+
+        int status = Robot.CONT;
+
+        if(firstTime == true) {
+            firstTime = false;
+            step = 1;
+        }
+
+        switch(step) {
+            case 1:     // Rotate arm to position(need to get this angle)
+                break;
+            case 2:     // Fully retract arm(need to confirm with austin)
+                break;
+            default:    // Finished routine, reset variables and return done
+                step = 1;
+                firstTime = true;
+                return Robot.DONE;
+        }
+
+        // Done current step, goto next one
+        if(status == Robot.DONE) {
+            step++;
+        }
+
+        return Robot.CONT;
+    }
+
+    /**
+     * Sets up and moves the arm to climb the chain
+     * 
+     * @return Robot.CONT or Robot.DONE
+     */
+    public int autoClimb() {
+        System.err.println("Not implemented yet!");
+
+        int status = Robot.CONT;
+
+        if(firstTime == true) {
+            firstTime = false;
+            step = 1;
+        }
+
+        switch(step) {
+            case 1:     // Rotate arm vertically
+                break;
+            case 2:     // Extend arm to mount chain
+                break;
+            case 3:     // Retract arm fully to climb 
+                break;
+            default:    // Finished routine, reset variables and return done
+                step = 1;
+                firstTime = true;
+                return Robot.DONE;
+        }
+
+        // Done current step, goto next one
+        if(status == Robot.DONE) {
+            step++;
+        }
+
+        return Robot.CONT;
     }
 
 
     /****************************************************************************************** 
-    *
-    *    HELPER FUNCTIONS
-    * 
-    ******************************************************************************************/
+     *
+     *    HELPER FUNCTIONS
+     * 
+     ******************************************************************************************/
     /**
      * Delays the program for a set number of seconds.
      * 
